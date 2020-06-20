0a28b213d93de1cd90f52c0c3884179afc392b2d
Martin Sun <sunhaif@gmail.com>
2020-06-19 23:36:57 -0600

    删除 el-标签

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,51 +1,50 @@
 <template>
   <div class="login-container">
-    <el-form v-if="showType == 'login'" class="form-wrapper">
+    <form v-if="showType == 'login'" class="form-wrapper">
       <div class="logo-wrapper">
         <img src="../assets/images/logo.png" alt="logo">
       </div>
-      <el-form-item>
-        <el-input type="text" v-model="email" placeholder="请输入邮箱"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input type="password" v-model="password" placeholder="请输入密码" @keyup.enter.native="handleSubmit"></el-input>
-      </el-form-item>
-      <el-form-item>
+      <div>
+        <input type="text" v-model="email" placeholder="请输入邮箱"/>
+      </div>
+      <div>
+        <input type="password" v-model="password" placeholder="请输入密码" @keyup.enter.native="handleSubmit"/>
+      </div>
+      <div>
         <div>
-          <el-button class="full-btn" type="primary" @click="handleSubmit">登录</el-button>
+          <button class="full-btn" type="primary" @click="handleSubmit">登录</button>
         </div>
         <div class="mt10">
-          <el-button class="full-btn" @click="showType = 'repwd'">重置密码</el-button>
+          <button class="full-btn" @click="showType = 'repwd'">重置密码</button>
         </div>
-      </el-form-item>
-    </el-form>
-    <el-form v-else class="form-wrapper">
+      </div>
+    </form>
+    <form v-else class="form-wrapper">
       <div class="logo-wrapper">
         <img src="../assets/images/logo.png" alt="logo">
       </div>
-      <el-form-item>
-        <el-input type="text" v-model="email" placeholder="请输入邮箱"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input type="password" v-model="password" placeholder="请输入新密码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input type="password" v-model="rePassword" placeholder="请再次输入新密码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input type="password" v-model="code" placeholder="请输入验证码">
-           <el-button slot="append" :disabled="codeDisabled" @click="handleSendRePwdCode">发送验证码</el-button>
-        </el-input>
-      </el-form-item>
-      <el-form-item>
+      <div>
+        <input type="text" v-model="email" placeholder="请输入邮箱"/>
+      </div>
+      <div>
+        <input type="password" v-model="password" placeholder="请输入新密码"/>
+      </div>
+      <div>
+        <input type="password" v-model="rePassword" placeholder="请再次输入新密码"/>
+      </div>
+      <div>
+        <input type="password" v-model="code" placeholder="请输入验证码"/>
+           <button slot="append" :disabled="codeDisabled" @click="handleSendRePwdCode">发送验证码</button>
+      </div>
+      <div>
         <div>
-          <el-button class="full-btn" type="primary" @click="handleRePwd">确定重置密码</el-button>
+          <button class="full-btn" type="primary" @click="handleRePwd">确定重置密码</button>
         </div>
         <div class="mt10">
-          <el-button class="full-btn" @click="showType = 'login'">返回</el-button>
+          <button class="full-btn" @click="showType = 'login'">返回</button>
         </div>
-      </el-form-item>
-    </el-form>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -143,29 +142,5 @@ export default {
 </script>
 
 <style>
-.full-btn{
-  width: 100%;
-}
 
-.login-container {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #06beb6;
-  background: linear-gradient(to right, #48b1bf, #06beb6);
-}
-
-.form-wrapper {
-  width: 320px;
-  box-sizing: border-box;
-  background: #fff;
-  padding: 20px 20px 0;
-  border-radius: 10px;
-}
-
-.logo-wrapper {
-  text-align: center;
-}
 </style>
