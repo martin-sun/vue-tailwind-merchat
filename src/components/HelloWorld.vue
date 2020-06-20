40f13fb2e725940d23cd0615b30f38ff352ecbce
Martin Sun <sunhaif@gmail.com>
2020-06-19 20:34:48 -0600

    add postcss-purge to control the css size

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <h1 class="text-red-500 text-6xl">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
